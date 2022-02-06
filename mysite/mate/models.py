c86d530ffea1f2b8ba6e17114bc46b64d3ddfdd8
iozalla <46322991+iozalla@users.noreply.github.com>
2022-02-06 15:05:43 +0100

    Continuado el tutorial

--- mysite/mate/models.py
+++ mysite/mate/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+###Aqui estan las tablas de la base de datos.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    age = models.IntegerField(default=10)
+    mail= models.CharField(max_length=200, primary_key=True )
+    def __str__(self):
+        return self.name
